239ad310a462d1e08e9f88bdbba85f441caab551
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2017-04-10 00:35:04 +0300

    Added use and sell
    These are methods that decrement the stock of ingredients/dishes.
    Reformatted code.

--- StockManagement.java
+++ StockManagement.java
@@ -9,23 +9,23 @@ public final class StockManagement {
         dishes = new Hashtable<>();
     }
 
-    public synchronized void addIngredient (Ingredient ingredient, Integer stock) {
+    public synchronized void addIngredient(Ingredient ingredient, Integer stock) {
         ingredients.put(ingredient, stock);
     }
 
-    public void addIngredient (Ingredient ingredient) {
+    public void addIngredient(Ingredient ingredient) {
         addIngredient(ingredient, 0);
     }
 
-    public synchronized void addDish (SushiDish sushiDish, Integer stock) {
+    public synchronized void addDish(SushiDish sushiDish, Integer stock) {
         dishes.put(sushiDish, stock);
     }
 
-    public void addDish (SushiDish sushiDish) {
+    public void addDish(SushiDish sushiDish) {
         addDish(sushiDish, 0);
     }
 
-    public synchronized void restockIngredient (Ingredient ingredient, Integer amount) {
+    public synchronized void restockIngredient(Ingredient ingredient, Integer amount) {
         if (ingredients.contains(ingredient)) {
             Integer stock = ingredients.get(ingredient) + amount;
             ingredients.put(ingredient, stock);
@@ -34,12 +34,44 @@ public final class StockManagement {
         }
     }
 
-    public synchronized void restockDish (SushiDish sushiDish, Integer amount) {
+    public synchronized void restockDish(SushiDish sushiDish, Integer amount) {
         if (dishes.contains(sushiDish)) {
             Integer stock = dishes.get(sushiDish) + amount;
             dishes.put(sushiDish, stock);
         } else {
             addDish(sushiDish, amount);
+        }
+    }
+
+    public synchronized void use(Ingredient ingredient, Integer amount) throws Exception {
+        if (ingredients.contains(ingredient) && amount > 0) {
+            Integer stock = ingredients.get(ingredient) - amount;
+
+            if (stock < 0) throw new Exception("Stock level cannot be negative!");      // TODO: Maybe create a custom Exception?
+
+            if (stock < ingredient.getRestockLevel()) {
+                System.out.println(ingredient.getName() + " has to be restocked!");
+            }
+
+            ingredients.put(ingredient, stock);
+        } else {
+            throw new Exception("Invalid method arguments!");
+        }
+    }
+
+    public synchronized void sell(SushiDish sushiDish, Integer amount) throws Exception {
+        if (dishes.contains(sushiDish) && amount > 0) {
+            Integer stock = dishes.get(sushiDish) - amount;
+
+            if (stock < 0) throw new Exception("Stock level cannot be negative");
+
+            if (stock < sushiDish.getRestockLevel()) {
+                System.out.println(sushiDish.getName() + " has to be restocked!");
+            }
+
+            dishes.put(sushiDish, stock);
+        } else {
+            throw new Exception("Invalid arguments!");
         }
     }
 }
