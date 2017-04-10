d81eca06ccf4d2b8343252e34e6da53604b8d795
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2017-04-10 12:09:07 +0300

    Tested StockManagement
    Fixed some mistakes

--- StockManagement.java
+++ StockManagement.java
@@ -26,7 +26,7 @@ public final class StockManagement {
     }
 
     public synchronized void restockIngredient(Ingredient ingredient, Integer amount) {
-        if (ingredients.contains(ingredient)) {
+        if (ingredients.containsKey(ingredient)) {
             Integer stock = ingredients.get(ingredient) + amount;
             ingredients.put(ingredient, stock);
         } else {
@@ -34,8 +34,8 @@ public final class StockManagement {
         }
     }
 
-    public synchronized void restockDish(SushiDish sushiDish, Integer amount) {
-        if (dishes.contains(sushiDish)) {
+    public synchronized void prepareDish(SushiDish sushiDish, Integer amount) {
+        if (dishes.containsKey(sushiDish)) {
             Integer stock = dishes.get(sushiDish) + amount;
             dishes.put(sushiDish, stock);
         } else {
@@ -44,13 +44,13 @@ public final class StockManagement {
     }
 
     public synchronized void use(Ingredient ingredient, Integer amount) throws Exception {
-        if (ingredients.contains(ingredient) && amount > 0) {
+        if (ingredients.containsKey(ingredient) && amount > 0) {
             Integer stock = ingredients.get(ingredient) - amount;
 
             if (stock < 0) throw new Exception("Stock level cannot be negative!");      // TODO: Maybe create a custom Exception?
 
             if (stock < ingredient.getRestockLevel()) {
-                System.out.println(ingredient.getName() + " has to be restocked!");
+                System.err.println(ingredient.getName() + " has to be restocked!");
             }
 
             ingredients.put(ingredient, stock);
@@ -60,13 +60,13 @@ public final class StockManagement {
     }
 
     public synchronized void sell(SushiDish sushiDish, Integer amount) throws Exception {
-        if (dishes.contains(sushiDish) && amount > 0) {
+        if (dishes.containsKey(sushiDish) && amount > 0) {
             Integer stock = dishes.get(sushiDish) - amount;
 
             if (stock < 0) throw new Exception("Stock level cannot be negative");
 
             if (stock < sushiDish.getRestockLevel()) {
-                System.out.println(sushiDish.getName() + " has to be restocked!");
+                System.err.println(sushiDish.getName() + " has to be restocked!");
             }
 
             dishes.put(sushiDish, stock);
